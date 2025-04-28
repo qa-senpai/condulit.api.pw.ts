5090c851be972d1285a67881bf0432a1e76d3ec5
qa_coach <psp@sitecore.net>
2025-04-28 20:42:58 +0300

    refactor: enhance logging for user registration request body in global setup

--- global.setup.ts
+++ global.setup.ts
@@ -15,6 +15,10 @@ export default async function globalSetup(config: FullConfig) {
       username: process.env.USERNAME!,
     };
 
+    console.log(requestBody.email);
+    console.log(requestBody.password);
+    console.log(requestBody.username);
+
     console.log("THIS IS MY ENV VARIABLE:");
     console.log(requestBody);
 
